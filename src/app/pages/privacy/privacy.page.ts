634f8ee35993b9fdc626f13a9476720c178918ee
Henrik Bolte <dev@hbolte.com>
2020-09-02 00:00:04 +0700

    use contentful in privacy page

--- src/app/pages/privacy/privacy.page.ts
+++ src/app/pages/privacy/privacy.page.ts
@@ -1,22 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {Observable} from 'rxjs';
-import {ISettings} from '../../core/interfaces/settings.interface';
-import {AngularFirestore} from '@angular/fire/firestore';
+import {ILegal} from '../../core/models/legal.interface';
+import {ContentfulService, ContentTypeLegal} from '../../core/contentful/provider/contentful.service';
 
 @Component({
   selector: 'app-privacy',
   templateUrl: './privacy.page.html',
   styleUrls: ['./privacy.page.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class PrivacyPage {
 
-  public settings$: Observable<ISettings>;
+  public policy$: Observable<ILegal>;
 
-  constructor(private afs: AngularFirestore) {
+  constructor(private contentfulService: ContentfulService) {
   }
 
   public ionViewDidEnter() {
-    this.settings$ = this.afs.doc<ISettings>('settings/app').valueChanges();
+    this.policy$ = this.contentfulService.getLegalEntry(ContentTypeLegal.PRIVACY);
+  }
+
+  public parseRichText(richText: any) {
+    return this.contentfulService.parseRichText(richText);
   }
 
 }
